572728e1d353defdb1c29e67753cb746f3baf651
Ezenwa Chigozie <stonesilver12@gmail.com>
2020-05-30 08:17:37 +0100

    added keyboard input functionality

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ let percentage = document.querySelector('[data-percentage]');
 let squareroot = document.querySelector('[data-squareroot]');
 let square = document.querySelector('[data-square]');
 let oneDivideX = document.querySelector('[data-one-divide-x]');
-let operationSelected = '';
+let operationSelected = Number();
 let calculated = false;
 
 // The solve Function to Handle the Calculation
 const SOLVE = (operator) => {
    if (firstoperand.value == '' || secondoperand.value == '') return;
-   let first = parseFloat(firstoperand.value);
-   let second = parseFloat(secondoperand.value);
+   let first = Number(parseFloat(firstoperand.value));
+   let second = Number(parseFloat(secondoperand.value));
    switch (operationSelected) {
       case '+':
          secondoperand.value = first + second;
@@ -28,10 +28,10 @@ const SOLVE = (operator) => {
       case '-':
          secondoperand.value = first - second;
          break;
-      case '÷':
+      case '/':
          secondoperand.value = first / second;
          break;
-      case '×':
+      case '*':
          secondoperand.value = first * second;
          break;
       default:
@@ -75,13 +75,8 @@ operationBtn.forEach((button) => {
       operationSelected = button.textContent;
       if (firstoperand.value !== '') {
          return (firstoperand.value =
-            firstoperand.value.slice(0, -2) +
-            `${operationSelected} `);
+            firstoperand.value.slice(0, -2) + `${operationSelected} `);
       }
-      // if (calculated) {
-      //    // calculated= !calculated
-      //    return firstoperand.value += ` ${operationSelected} `;
-      // }
       firstoperand.value = `${secondoperand.value}  ${operationSelected} `;
       secondoperand.value = '';
    });
@@ -98,7 +93,7 @@ allClear.addEventListener('click', () => {
 });
 
 clear.addEventListener('click', () => {
-   if(secondoperand.value == '') return
+   if (secondoperand.value == '') return;
    secondoperand.value = secondoperand.value.toString().slice(0, -1);
    firstoperand.value = firstoperand.value.toString().slice(0, -1);
 });
@@ -108,27 +103,77 @@ calculateBtn.addEventListener('click', SOLVE);
 percentage.addEventListener('click', () => {
    secondoperand.value === ''
       ? ''
-      : (secondoperand.value = secondoperand.value / 100)
-      calculated = true;
+      : (secondoperand.value = secondoperand.value / 100);
+   calculated = true;
 });
 
 squareroot.addEventListener('click', () => {
    secondoperand.value === ''
-   ? ''
-   : (secondoperand.value = Math.sqrt(secondoperand.value))
+      ? ''
+      : (secondoperand.value = Math.sqrt(secondoperand.value));
    calculated = true;
 });
 
 square.addEventListener('click', () => {
    secondoperand.value === ''
       ? ''
-      : (secondoperand.value = Math.pow(secondoperand.value, 2))
-      calculated = true;
+      : (secondoperand.value = Math.pow(secondoperand.value, 2));
+   calculated = true;
 });
 
 oneDivideX.addEventListener('click', () => {
    secondoperand.value === ''
       ? ''
-      : (secondoperand.value = 1 / secondoperand.value)
-      calculated = true;
+      : (secondoperand.value = 1 / secondoperand.value);
+   calculated = true;
+});
+
+window.addEventListener('keydown', (event) => {
+   const numRegex = /^[0-9.]*$/;
+   const operatorRegex = /^[-+/*]*$/;
+   const keydown = event.key;
+
+   if (numRegex.test(keydown.toString())) {
+      (() => {
+         // Checking for duplicate dot(.) in the secondoperand
+      if (keydown == '.' && secondoperand.value.includes('.')) {
+         return;
+      }
+      // Checking if a solve function has fired to clear the secondoperand
+      if (calculated) {
+         secondoperand.value = '';
+         calculated = !calculated;
+      }
+      // Adding clicked button values to the secondoperand
+      secondoperand.value += keydown;
+      if (firstoperand.value !== '') {
+         firstoperand.value += `${keydown}`;
+      }
+      // Popping out multiple zero as the first number on the secondoperand
+      if (secondoperand.value.charAt() == '0' && secondoperand.value[1] >= 0) {
+         secondoperand.value = secondoperand.value.slice(1);
+      }
+      })()
+   }
+
+   if (operatorRegex.test(keydown.toString())) {
+      (() => {
+         if (secondoperand.value == '' && firstoperand.value == '') return;
+         if (firstoperand.value != '' && secondoperand.value != '') {
+            operationSelected = operationSelected;
+            SOLVE();
+         }
+         operationSelected = keydown;
+         if (firstoperand.value !== '') {
+            return (firstoperand.value =
+               firstoperand.value.slice(0, -2) + `${operationSelected} `);
+         }
+         firstoperand.value = `${secondoperand.value}  ${operationSelected} `;
+         secondoperand.value = '';
+      })();
+   }
+
+   if (keydown === 'Enter') {
+      SOLVE()
+   }
 });
